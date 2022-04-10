00a285fac4c1f58faffa8b50541f903e85b6a410
sobhanessi <s.esfandyari@covis.de>
2022-04-11 00:43:51 +0300

    finished the project

--- src/components/NavbarMiddleLinks.jsx
+++ src/components/NavbarMiddleLinks.jsx
@@ -1,67 +1,67 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import logo from "./pics/Logo 1.png";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from './pics/Logo 1.png';
 
 function NavbarMiddleLinks() {
   const styles = {
     navbar: {
-      position: "absolute",
-      top: "10%",
-      display: "flex",
-      justifyContent: "space-around",
-      textDecoration: "none",
-      width: "100%",
-      top: "80px",
+      position: 'absolute',
+      display: 'flex',
+      textDecoration: 'none',
+      width: '100%',
+      top: '80px',
+      left: '18%',
+      // justifyContent: 'space-around',
     },
     middleLinks: {
-      display: "flex",
-      flexDirection: "row",
-      alignItems: "center",
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
     },
     link: {
-      textDecoration: "none",
-      fontFamily: "Roboto",
-      fontStyle: "normal",
-      fontWeight: "400",
-      fontSize: "17px",
-      lineHeight: "150%",
-      color: "white",
+      textDecoration: 'none',
+      fontFamily: 'Roboto',
+      fontStyle: 'normal',
+      fontWeight: '400',
+      fontSize: '17px',
+      lineHeight: '150%',
+      color: 'white',
     },
     signUp: {
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      backgroundColor: "#F44E77",
-      padding: "10px",
-      borderRadius: "20px",
-      width: "167px",
-      height: "56px",
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#F44E77',
+      padding: '10px',
+      borderRadius: '20px',
+      width: '167px',
+      height: '56px',
     },
   };
   return (
-    <nav style={styles.navbar}>
-      <span style={{ ...styles.middleLinks }}>
-        <Link to="/home">
+    <div style={styles.navbar}>
+      <span style={{ ...styles.middleLinks, marginRight: '190px' }}>
+        <Link to='/home'>
           <img src={logo} />
         </Link>
       </span>
-      <span style={{ ...styles.middleLinks }}>
-        <Link to="/home" style={{ ...styles.link, paddingRight: "70px" }}>
+      <span style={{ ...styles.middleLinks, marginRight: '220px' }}>
+        <Link to='/home' style={{ ...styles.link, paddingRight: '70px' }}>
           Home
         </Link>
-        <Link to="/feature" style={{ ...styles.link, paddingRight: "70px" }}>
+        <Link to='/feature' style={{ ...styles.link, paddingRight: '70px' }}>
           Feature
         </Link>
-        <Link to="/pricing" style={{ ...styles.link }}>
+        <Link to='/pricing' style={{ ...styles.link }}>
           Pricing
         </Link>
       </span>
       <span style={styles.signUp}>
-        <Link to="/sign-up" style={{ ...styles.signUpLink, ...styles.link }}>
+        <Link to='/sign-up' style={{ ...styles.signUpLink, ...styles.link }}>
           Sign Up
         </Link>
       </span>
-    </nav>
+    </div>
   );
 }
 
